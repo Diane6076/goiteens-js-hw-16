9bf9750e7f25b131fee7e255025d25a0db83f925
diana <dskripka005@gmail.com>
2023-10-14 12:44:25 +0300

    sixandfour

--- js/main.js
+++ js/main.js
@@ -1,69 +1,65 @@
 import users from './users.js'
+console.log(users);
 
 //*1
 //Отримати масив імен всіх користувачів (поле name).
 
 const getUserNames = users => {
-  
+  return users.map(user => user.name)
 };
 
 console.log(getUserNames(users));
-// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
 
 //*2
 //Отримати масив об'єктів користувачів за кольором очей (поле eyeColor).
 
 const getUsersWithEyeColor = (users, color) => {
-  
+  return users.filter(user => user.eyeColor === color);
 };
 
 console.log(getUsersWithEyeColor(users, 'blue')); 
-// [об'єкт Moore Hensley, об'єкт Sharlene Bush, об'єкт Carey Barr]
 
 //*3
 //Отримати масив імен користувачів за статтю (поле gender).
 
 const getUsersWithGender = (users, gender) => {
- 
+  // return users.filter(user => user.gender === gender)
+  // .map(user => user.name);
 };
 
-console.log(getUsersWithGender(users, 'male'));
- // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+//console.log(getUsersWithGender(users, 'male'));
 
 //*4
 //Отримати масив тільки неактивних користувачів (поле isActive).
 
 const getInactiveUsers = users => {
-  
+  return users.filter(user => !user.isActive);
 };
-  
 console.log(getInactiveUsers(users)); 
-  // [об'єкт Moore Hensley, об'єкт Ross Vazquez, об'єкт Blackburn Dotson]
 
 //*5
 //Отримати користувача (не масив) по email (поле email, він унікальний).
 
 const getUserWithEmail = (users, email) => {
-   
-  };
+ return users.find( (user) => {
+    // const userFind
+  if (user.email === email) {
+    return user
+  }} ) 
+};
   
 console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); 
-  // {об'єкт користувача Sheree Anthony}
-console.log(getUserWithEmail(users, 'elmahead@omatom.com')); 
-  // {об'єкт користувача Elma Head}
+// {об'єкт користувача Sheree Anthony}
+//console.log(getUserWithEmail(users, 'elmahead@omatom.com')); 
+// {об'єкт користувача Elma Head}
   
   
 //*6
 // Отримати масив користувачів, які потрапляють у вікову категорію від min до max років (поле age).
-  
 const getUsersWithAge = (users, min, max) => {
- 
-  };
-  
-console.log(getUsersWithAge(users, 20, 30));
- // [об'єкт Ross Vazquez, об'єкт Elma Head, об'єкт Carey Barr]
+  const usersFilter = users.filter(user => user.age >= min && user.age <= max)
+  return usersFilter
+};
+
+console.log(getUsersWithAge(users, 20, 30)); 
 console.log(getUsersWithAge(users, 30, 40));
-  // [об'єкт Moore Hensley, об'єкт Sharlene Bush, об'єкт Blackburn Dotson, об'єкт Sheree Anthony]
-
-
-
